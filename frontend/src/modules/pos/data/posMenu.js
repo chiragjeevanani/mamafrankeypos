8230afe16c274843a7dfc263d49dedbc9faeb86e
Sakshi Dwivedi <sakshidwivedi406@gmail.com>
2026-04-13 11:54:00 +0530

    hardcoded menu items removed

--- frontend/src/modules/pos/data/posMenu.js
+++ frontend/src/modules/pos/data/posMenu.js
@@ -7,18 +7,5 @@ export const POS_CATEGORIES = [
   { id: 'soft', name: 'Soft Drinks', icon: 'CupSoda', color: '#F44336' },
 ];
 
-export const POS_MENU_ITEMS = [
-  { id: '1', catId: 'cat3', name: 'Chaas', price: 40, code: '101', shortcut: 'CH', image: '', variants: [{ id: 'v1', name: 'Half', price: 25 }, { id: 'v2', name: 'Full', price: 40 }] },
-  { id: '2', catId: 'cat3', name: 'Chai', price: 25, code: '102', shortcut: 'CI', image: '' },
-  { id: '3', catId: 'cat3', name: 'Coffee', price: 60, code: '103', shortcut: 'CO', image: '', variants: [{ id: 'v3', name: 'Half', price: 35 }, { id: 'v4', name: 'Full', price: 60 }] },
-  { id: '4', catId: 'cat3', name: 'Limbu Paani', price: 45, code: '104', shortcut: 'LP', image: '' },
-  { id: '5', catId: 'cat3', name: 'Soda', price: 30, code: '105', shortcut: 'SO', image: '' },
-  { id: '6', catId: 'cat3', name: 'Thandai', price: 80, code: '106', shortcut: 'TH', image: '' },
-  { id: '7', catId: 'cat3', name: 'Virgin Pina Colada', price: 150, code: '107', shortcut: 'VPC', image: '' },
-  
-  { id: 'i1', catId: 'cat2', name: 'Paneer Tikka', price: 240, code: '201', shortcut: 'PT', image: '' },
-  { id: 'i2', catId: 'cat2', name: 'Dal Makhani', price: 180, code: '202', shortcut: 'DM', image: '' },
-  { id: 'i3', catId: 'cat2', name: 'Butter Naan', price: 50, code: '203', shortcut: 'BN', image: '', variants: [{ id: 'v5', name: 'Half', price: 30 }, { id: 'v6', name: 'Full', price: 50 }] },
-  { id: 'i4', catId: 'cat2', name: 'Paneer Butter Masala', price: 280, code: '69', shortcut: 'PBM', image: '' },
-];
+export const POS_MENU_ITEMS = [];
 
